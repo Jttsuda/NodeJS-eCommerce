bade6409317337eb559d48677af5410e887c53c2
Jared Suda <jtssuda@hotmail.com>
2021-04-21 06:11:58 -0600

    Added media queries and admin permissions

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,6 +1,8 @@
 const Product = require('../models/product')
+const fs = require('fs');
 
 
+// Products Page
 const product_index = (req, res) => {
     Product.find()
         .then((result) => {
@@ -12,6 +14,7 @@ const product_index = (req, res) => {
 }
 
 
+// Product Details Page
 const product_details = (req, res) => {
     Product.findById(req.params.id)
         .then((result) => {
@@ -24,4 +27,26 @@ const product_details = (req, res) => {
 }
 
 
-module.exports = { product_index, product_details }
+// Delete a Product and Images
+const product_delete = async (req, res) => {
+    let product = await Product.findById(req.params.id);
+    fs.unlink('./public/images/products/' + product.image[0], (err) => {
+        if (err) {
+            throw err;
+        }});
+    fs.unlink('./public/images/products/' + product.image[1], (err) => {
+        if (err) {
+            throw err;
+        }});
+    Product.findByIdAndDelete(req.params.id)
+        .then((result) => {
+            res.json({ redirect: '/products' });
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(404).render('404', {});
+        });
+}
+
+
+module.exports = { product_index, product_details, product_delete }
--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { product_index, product_details } = require('../controllers/productController');
+const { product_index, product_details, product_delete } = require('../controllers/productController');
 
 
-// Comment Routes
+// Product Routes
 router.get('/', product_index);
 router.get('/:id', product_details);
+router.delete('/:id', product_delete);
 // router.route('/:id').get(comment_details).delete(comment_delete);
 
 
